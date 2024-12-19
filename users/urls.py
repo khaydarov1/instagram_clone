a652dab2aa8b363a8cf842769cbc4393a7a33131
j.haydarov <khaydarovjahongir02@gmail.com>
2024-12-19 17:44:30 +0500

    Reset Password

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import CreateUserView, VerifyAPIView, GetNewVerification, \
     ChangeUserInformationView, ChangeUserPhotoView, LoginView,\
-    LoginRefreshView,LogOutView
+    LoginRefreshView,LogOutView,ForgotPasswodView,ResetPasswordView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
     path('login/refresh/', LoginRefreshView.as_view()),
+    path('logout/',LogOutView.as_view()),
     path('signup/', CreateUserView.as_view(), name='create_user'),
     path('verify/', VerifyAPIView.as_view(), name='verify_user'),
     path('new-verify/', GetNewVerification.as_view(), name='new_verify_user'),
