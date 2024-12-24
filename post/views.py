a7fc8c5383c6096de0a17264248295817906f51f
khaydarov1 <jhaydarov2002@gmail.com>
2024-12-24 16:07:26 +0500

    Custom Pagination

--- post/views.py
+++ post/views.py
@@ -2,11 +2,12 @@ from .models import CommentLike, Post, PostComment, PostLike
 from .serializers import PostSerializers, CommentSerializer, PostLikeSerializer, CommentLikeSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
-
+from shared.custom_pagination import CustomPagination
 
 class PostListAPIView(generics.ListAPIView):
     serializer_class= PostSerializers
     permission_classes=[AllowAny, ]
+    pagination_class=CustomPagination
     
     def get_queryset(self):
         return Post.objects.all()
